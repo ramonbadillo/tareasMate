06e9524aa877af087ef63e86a2324e388a6ba94f
maria juana mota <juanis315@hotmail.com>
2014-10-17 15:06:08 -0500

    Ejercicios Faltantes

--- tarea1.hs
+++ tarea1.hs
@@ -1,6 +1,9 @@
 #!/usr/bin/env runhaskell
 
-
+xor :: Bool -> Bool -> Bool
+xor x y | x == True && y == False = True
+        | x == False && y == True = True
+        | otherwise = False
 
 equiv3 ::  (Bool ->Bool -> Bool ->Bool) -> (Bool->Bool->Bool->Bool) -> Bool
 equiv3 func1 func2 = and [ 
@@ -16,10 +19,19 @@ equiv2 func1 func2 = and [
 	]
 
 main = print (equiv3 
-	 	(\ p q r -> (p <= q ) <= r)
-	 	(\ a b c-> a <= ( b <= c)),
+	 	(\ p q r -> (p <= q ) <= r) --15
+	 	(\ a b c-> a <= ( b <= c)),	
+	 	equiv2
+ 		(\ p q -> ( p <= q )) 	-- 16
+ 		(\ a b -> ( not b ) <= (not a)),
+ 		equiv2
+ 		(\ p q -> (not p) ==  q ) 	-- 17
+ 		(\ a b -> a ==(not b)),
+ 		equiv2
+ 		(\ p q -> not (xor p q)) 	-- 18
+ 		(\ a b -> a ==b), 	
 		equiv2
- 		(\ p q  -> not q <= not p)
+ 		(\ p q  -> not q <= not p)--19
 		(\ a b ->  a <= b),
 		equiv3 --20
 		(\ p q r -> (p <= q ) && (p <= r ))
@@ -36,12 +48,12 @@ main = print (equiv3
 		equiv3 --24
 		(\ p q r -> not p <= ( q <= r ) )
 		(\ a b c -> b <= ( a || c ) ),
-		equiv3 --25
-		(\ p q r -> (p <= r ) || (q <= r ))
-		(\ a b c -> (a || b) <= c),
-		equiv3 --26
-		(\ p q r -> (p <= r ) || (q <= r ))
-		(\ a b c -> (a || b) <= c)
+		equiv2 --25
+		(\ p q  -> p == q)
+		(\ a b -> (a <= b) && (b <= a)),
+		equiv2 --26
+		(\ p q -> p == q )
+		(\ a b -> (not a) == (not b))
 		
 		)
 	
@@ -53,7 +65,7 @@ main = print (equiv3
 --PROBLEMA 17-------------------
 valid17A :: (Bool -> Bool -> Bool) -> Bool
 valid17A func = and [func a b | a <- [True,False] , b <- [True,False]  ]
-test17A p q = not q <$> p 
+
 
 valid17B :: (Bool -> Bool -> Bool) -> Bool
 valid17B func = and [func a b | a <- [True,False] , b <- [True,False]  ]
